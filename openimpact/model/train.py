77768796a0cd7d9509ed58cd07caeddf77be4032
Florian Hammer <florianmhammer@gmail.com>
2025-01-23 12:02:22 +0100

    ENH: Added MLFlowLogger

--- openimpact/model/train.py
+++ openimpact/model/train.py
@@ -6,8 +6,12 @@ from torch_geometric.data import InMemoryDataset  # type: ignore
 from torch_geometric.loader import DataLoader  # type: ignore
 from lightning.pytorch.loggers import CSVLogger
 
+from torch_geometric.nn.summary import summary
+
 from openimpact.data.datasets import train_test_split
-from openimpact.model.gnn import FarmGNN, FarmGNN3, FarmGAT
+from openimpact.model.gnn import FarmGAT
+
+from lightning.pytorch.loggers import MLFlowLogger
 
 
 def train_gnn(
@@ -19,13 +23,8 @@ def train_gnn(
     experiment: str = "GNN",
 ):
     """
-    currently testing this combination:
-
-    h_dims = [256, 256, 256]
-    heads = [8, 8, 8]
-    concats = [True, True, True]
-    edge_dims = [2, None, None]
-    skip_connections = [True, True, True]
+    General training function that uses DataLoaders, the lightning Trainer class
+    as well as the CSVLogger and MLFlowLogger classes.
     """
     train_data, val_data = train_test_split(dataset)
 
@@ -45,10 +44,32 @@ def train_gnn(
         **gnn_params,
     )
 
-    logger = CSVLogger(log_dir, f"{experiment}")
+    # Initialise the bias of the last layer (MLP)
+    model.mp[0].lins[-1].bias.data.fill_(6.8)
+
+    mlf_logger = MLFlowLogger(
+        experiment_name="lightning_logs",
+        tracking_uri="file:./ml-runs",
+        log_model=True,
+    )
+    run_id = mlf_logger.run_id
+
+    x = train_data[0].x
+    edge_index = train_data[0].edge_index
+    edge_attr = train_data[0].edge_attr
+
+    model_summary = str(summary(model, (x, edge_index, edge_attr)))
+
+    mlf_logger.experiment.log_text(run_id, model_summary, "model_summary.txt")
+    mlf_logger.experiment.set_tag(run_id, "Model", str(model.__class__))
+    mlf_logger.experiment.set_tag(run_id, "Dataset", str(dataset))
+
+    csv_logger = CSVLogger(log_dir, f"{experiment}")
+
+    loggers = [csv_logger, mlf_logger]
     max_epochs = train_params.get("max_epochs", 200)
     trainer = L.Trainer(
-        max_epochs=max_epochs, logger=logger, accelerator="gpu"
+        max_epochs=max_epochs, logger=loggers, accelerator="gpu"
     )
     trainer.fit(
         model=model, train_dataloaders=train_loader, val_dataloaders=val_loader
